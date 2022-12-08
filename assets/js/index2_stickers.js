6eaa311f380c26b04bcbf79c259010e9ed0026bc
AngeloPaolo23 <angelopaolo0223@gmail.com>
2022-12-08 16:50:09 -0300

    DEFINICION DE VARIABLES JS

---
+++ assets/js/index2_stickers.js
@@ -0,0 +1,8 @@
+primerInput = document.querySelector("#input1").value;
+segundoInput = document.querySelector("#input2").value;
+tercerInput = document.querySelector("#input3").value;
+boton = document.querySelector("button");
+
+
+
+boton.addEventListener("click", )
