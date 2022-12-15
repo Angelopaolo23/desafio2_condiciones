790b7edc87bee66905e0a94b758b804ba4b1d3a7
AngeloPaolo23 <angelopaolo0223@gmail.com>
2022-12-15 00:39:41 -0300

    CREACION DE FUNCION CON CONDICIONES

--- assets/js/index2_stickers.js
+++ assets/js/index2_stickers.js
@@ -1,8 +1,19 @@
-primerInput = document.querySelector("#input1").value;
-segundoInput = document.querySelector("#input2").value;
-tercerInput = document.querySelector("#input3").value;
-boton = document.querySelector("button");
+function sumar() {
+  const primerInput = document.querySelector("#input1").value;
+  const segundoInput = document.querySelector("#input2").value;
+  const tercerInput = document.querySelector("#input3").value;
+  const cantidadTotal = parseInt(primerInput) + parseInt(segundoInput) + parseInt(tercerInput);
+  if (cantidadTotal == 0) {
+    document.querySelector("#respuesta_cantidad").innerHTML = "No llevas ningun sticker, ingresa una cantidad de los que necesites si es que deseas comprar."
+  } else if (cantidadTotal > 10) {
+    document.querySelector("#respuesta_cantidad").innerHTML = "Llevas demasiados stickers, no tenemos tanto stock."
+  } else{
+    document.querySelector("#respuesta_cantidad").innerHTML = `Llevas un total de ${cantidadTotal} stickers.`
+  }
+  }
+  
 
 
 
-boton.addEventListener("click", )
+
+
